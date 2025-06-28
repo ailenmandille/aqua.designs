8202da84d23c8021d14410a1ec119784b7e69422
Ailen Mandille <ailenmandille@gmail.com>
2025-06-28 20:33:55 -0300

    creo el repositorio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// repeticion condicional while
+
+let iteracion = 1;
+
+while (iteracion <= 5) {
+    console.log(iteracion);
+    iteracion++;
+}
+
+// arrays o listas
+let productos = [ "laptop", "celular", "tablet"];
+console.log (productos)
+
+productos.push ("televisor");
+console.log (productos)
